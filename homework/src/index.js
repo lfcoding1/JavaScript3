e7590157ce86a3b376cdf7e5fc80dcf2ee72a187
Louise Francois <louise.ban@gmail.com>
2018-11-16 09:48:38 +0100

    change to homework

--- homework/src/index.js
+++ homework/src/index.js
@@ -10,6 +10,7 @@
         cb(null, xhr.response);
       } else {
         cb(new Error(`Network error: ${xhr.status} - ${xhr.statusText}`));
+        console.log("Hello error");
       }
     };
     xhr.onerror = () => cb(new Error('Network request failed'));
@@ -30,7 +31,6 @@
     return elem;
   }
   
-
   
   function main(url) {
     while (root.firstChild) {
@@ -41,58 +41,67 @@
       let newArray = [];
       let forkArray = [];
       let languageArray = [];
+      let descriptionArray = [];
       let htmlArray = [];
       let contributorsArray = [];
       for (let i = 0; i < data.length; i++){
-        newArray.push(data[i].name);
-        newArray.sort();
-        forkArray.push(data[i].forks);
-        languageArray.push(data[i].language);
-        contributorsArray.push(data[i].contributors_url);
-        contributorsArray.sort();
-        htmlArray.push(data[i].html_url);
-        htmlArray.sort();
-      }
-   
-    let app = document.getElementById('root');   
-    const header = createAndAppend('h1', app, { text: "Hack Your Future Repositories", class: 'title' });
-    const subHeader = createAndAppend('h3', app, { text: "Select a repository:  ", class: 'subtitle'});
-    const selectList = createAndAppend('select', app, { text: 'Select a Repo', id: "mySelect" });
-    const container = createAndAppend('div', app, {class: 'container'});
-    const card = createAndAppend('div', container, {text: "Description of this repository ", class: 'card'});
-    const ul = createAndAppend('ul', card, {id: "myUl", });
-    const contributorsheader = createAndAppend('h1', root, { text: "Contributors", class: 'title' });
-    const contributorsContainer = createAndAppend('div', root, { class: 'container'})
-    const contributorsCard = createAndAppend('div', contributorsContainer, {text: "Contributors to this Repository", class: 'card'});
-    const contributorsUl = createAndAppend('ul', contributorsCard, {id: 'contributorsUl'});
+          newArray.push(data[i].name);
+          newArray.sort();
+          descriptionArray.push(data[i].description);
+          forkArray.push(data[i].forks);
+          languageArray.push(data[i].language);
+          contributorsArray.push(data[i].contributors_url);
+          contributorsArray.sort();
+          htmlArray.push(data[i].html_url);
+          htmlArray.sort(); 
+       }
+         
+      let app = document.getElementById('root');   
+      const header = createAndAppend('h1', app, { text: "Hack Your Future Repositories", class: 'title' });
+      const subHeader = createAndAppend('h3', app, { text: "Select a repository:  ", class: 'subtitle'});
+      const selectList = createAndAppend('select', app, { text: 'Select a Repo', id: "mySelect" });
+      const container = createAndAppend('div', app, {class: 'container'});
+      const card = createAndAppend('div', container, {text: "Information about this repository: ", class: 'card'});
+      const ul = createAndAppend('ul', card, {id: "myUl", });
+      const contributorsheader = createAndAppend('h1', root, { text: "Contributors", class: 'title' });
+      const contributorsContainer = createAndAppend('div', root, { class: 'container'})
+      const contributorsCard = createAndAppend('div', contributorsContainer, {text: "Contributors to this Repository", class: 'card'});
+      const contributorsUl = createAndAppend('ul', contributorsCard, {id: 'contributorsUl'});
   
    
     
-    data.forEach((repo) => {  
-      for (let i = 0; i < newArray.length; i++) {
-        createAndAppend('option', selectList, {id: "myOption", value: i, text: newArray[i]});
-        }
-      });
+      data.forEach((repo) => {  
+        for (let i = 0; i < newArray.length; i++) {
+            createAndAppend('option', selectList, {id: "myOption", value: i, text: newArray[i]});
+          }
+        });
     
-    function removeNodes(container){
-        while (ul.hasChildNodes()) {
-          ul.removeChild(ul.firstChild);
+      function removeNodes(container){
+            while (ul.hasChildNodes()) {
+                ul.removeChild(ul.firstChild);
+          }
+            while (contributorsUl.hasChildNodes()) {
+                contributorsUl.removeChild(contributorsUl.firstChild);
+          }
         }
-        while (contributorsUl.hasChildNodes()) {
-          contributorsUl.removeChild(contributorsUl.firstChild);
-        }
-      }
 
-    selectList.onchange = function(selectedIndex){
-      createAndAppend('li', ul, { text: "Repository: " + newArray[this.selectedIndex], class: 'nameInContainer', function: removeNodes()});
-      createAndAppend('a', ul, { text: htmlArray[this.selectedIndex], id: 'linkInContainer', target: "_blank", href: htmlArray[this.selectedIndex]});
-      createAndAppend('li', ul, { text: "Number of Forks: " + forkArray[this.selectedIndex], class: 'forksInContainer'});
-      createAndAppend('li', ul, { text: "Language: " + languageArray[this.selectedIndex], class: 'languageInContainer'});
-      createAndAppend('li', contributorsUl, { text: contributorsArray[this.selectedIndex], class: 'contributorsInContainer'});
-      }
+      selectList.onchange = function(selectedIndex){
+          let RepoName = createAndAppend('li', ul, { text: "Repository: ", class: 'nameInContainer', function: removeNodes()});
+          createAndAppend('a', RepoName, { text: newArray[this.selectedIndex], id: 'linkInContainer', target: "_blank", href: htmlArray[this.selectedIndex]});
+          createAndAppend('li', ul, {text: "Description: " + descriptionArray[this.selectedIndex], class: 'descriptionInContainer'});
+          createAndAppend('li', ul, { text: "Number of Forks: " + forkArray[this.selectedIndex], class: 'forksInContainer'});
+          createAndAppend('li', ul, { text: "Language: " + languageArray[this.selectedIndex], class: 'languageInContainer'});
+          createAndAppend('li', contributorsUl, { text: contributorsArray[this.selectedIndex], class: 'contributorsInContainer'});
+          }
+        });
+    /*
+    fetchJSON( theContributors_URL, (err, data) => {
+      let theContributorsUrl = [];
+      for (let i = 0; i < data.length; i++){
 
-    });
-  }
+      }
+    });*/
+    }
 
   const HYF_REPOS_URL = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
   const theContributors_URL = 'https://api.github.com/repos/HackYourFuture/contributors';
